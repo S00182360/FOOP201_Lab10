12ba624fd0ceb6447d19468895c4d159459c1013
Samuel Colville <S00182360@mail.itsligo.ie>
2018-12-04 15:14:29 +0000

    Layout Basic add/remove function added

---
+++ FOOP201_Lab10/Bike.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOOP201_Lab10
+{
+    class Bike
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Gender { get; set; }
+
+        public Bike(int productID, string name, decimal price, string gender)
+        {
+            ProductID = productID;
+            Name = name;
+            Price = price;
+            Gender = gender;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}\t{1}\t€{2:0.00}\t{3}", ProductID, Name, Price, Gender);
+        }
+    }
+}
--- FOOP201_Lab10/MainWindow.xaml.cs
+++ FOOP201_Lab10/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,57 @@ namespace FOOP201_Lab10
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Bike> BikeList = new ObservableCollection<Bike>();
+        ObservableCollection<Bike> ProductList = new ObservableCollection<Bike>();
+        ObservableCollection<Bike> CartList = new ObservableCollection<Bike>();
+        static decimal total = 0m;
+        string totalMsg = "";
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MainWindow1_Loaded(object sender, RoutedEventArgs e)
+        {
+            Bike bike1 = new Bike(1234, "Road Bike", 335.00m, "Male");
+            Bike bike2 = new Bike(1235, "Road Bike", 325.00m, "Female");
+            Bike bike3 = new Bike(1325, "Mountain Bike", 435.00m, "Male");
+            Bike bike4 = new Bike(1326, "Mountain Bike", 433.00m, "Female");
+            Bike bike5 = new Bike(2176, "Hybrid Bike", 735.00m, "Male");
+            Bike bike6 = new Bike(2178, "Hybrid Bike", 730.00m, "Female");
+
+            BikeList.Add(bike1);
+            BikeList.Add(bike2);
+            BikeList.Add(bike3);
+            BikeList.Add(bike4);
+            BikeList.Add(bike5);
+            BikeList.Add(bike6);
+
+            lbxProducts.ItemsSource = BikeList;
+            lbxCart.ItemsSource = CartList;
+
+            tbkCart.Text = totalMsg + total.ToString();
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Bike BikeSelected = lbxProducts.SelectedItem as Bike;
+            if (BikeSelected != null)
+            {
+                CartList.Add(BikeSelected);
+                total += BikeSelected.Price;
+            }
+        }
+
+        private void BtnRemove_Click(object sender, RoutedEventArgs e)
+        {
+            Bike BikeRemove = lbxCart.SelectedItem as Bike;
+            if (BikeRemove != null)
+            {
+                CartList.Remove(BikeRemove);
+                total -= BikeRemove.Price;
+            }
         }
     }
 }
